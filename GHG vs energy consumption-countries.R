4b9354bcbc8887772be14c5fdf2ee5cf421ee279
Shayan Jamil <75214997+mohamedshayan@users.noreply.github.com>
2020-12-22 21:04:47 -0700

    Create GHG vs energy consumption-countries.R

---
+++ GHG vs energy consumption-countries.R
@@ -0,0 +1,19 @@
+library(Benchmarking)
+library(psych)
+library(ggplot2)
+dataset <- read.csv("/Users/shayan/Documents/DEA_GHG/Energy consumption and GHG emissions.csv", header=TRUE)
+dataset
+summary(dataset)
+x <-dataset$energy_consumption_p.e
+y <-dataset$GHG_emission_p.e
+x
+y
+dea(x,y,RTS="vrs", ORIENTATION = "in")
+dearesults <- dea(x,y,RTS="vrs", ORIENTATION = "in")
+dearesults
+dea.plot(x,y, RTS = "vrs", ORIENTATION= "in-out")
+dea.plot.frontier(x,y,txt=data$Country)
+dea.plot.frontier(x,y,txt=data$Country,data$Country,xlab="MWh/capita", ylab="CO2/capita")
+
+
+
